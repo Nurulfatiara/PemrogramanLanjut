613aec99abd9e02cbe12a40306a23a47782bffa6
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-01 23:02:18 +0700

    Menggunakan perulangan for

---
+++ src/SoloLearn/ConditionalAndLoop/FoorLoops2.java
@@ -0,0 +1,9 @@
+package SoloLearn.ConditionalAndLoop;
+
+public class FoorLoops2 {
+    public static void main(String[] args) {
+        for (int i = 2; i < 10; i = i*i){
+            System.out.println(i);
+        }
+    }
+}
