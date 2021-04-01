c09e71249d064b786c7572518224e0ac214820d3
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-01 22:26:37 +0700

    Decision Making ==

---
+++ src/SoloLearn/ConditionalAndLoop/DecisionMaking.java
@@ -0,0 +1,9 @@
+package SoloLearn.ConditionalAndLoop;
+
+public class DecisionMaking {
+    public static void main(String[] args) {
+        int x = 5;
+        if (x==5);
+        System.out.println("Yeah");
+    }
+}
