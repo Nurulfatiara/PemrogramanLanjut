9dfb1b9946db311ce5c0303cd316feaf699ba9e6
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-01 22:43:59 +0700

    Statement if bersarang

---
+++ src/SoloLearn/ConditionalAndLoop/NestedIfStatement.java
@@ -0,0 +1,12 @@
+package SoloLearn.ConditionalAndLoop;
+
+public class NestedIfStatement {
+    public static void main(String[] args) {
+        int x = 37;
+        if(x>22){
+            if(x>31){
+                System.out.println("it works");
+            }
+        }
+    }
+}
