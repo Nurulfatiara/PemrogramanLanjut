d4c6e387db490ab049882b1eb56d21ea6cffa3b9
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-01 21:22:42 +0700

    Quiz 2

---
+++ src/SoloLearn/BasicConcepts/Quiz2.java
@@ -0,0 +1,9 @@
+package SoloLearn.BasicConcepts;
+
+public class Quiz2 {
+    public static void main(String[] args) {
+        String name;
+        name = "David";
+        System.out.println(name);
+    }
+}
