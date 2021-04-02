64de0c2113836e55015afc9b88dcad6736afab99
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-02 22:14:15 +0700

    Array Multidimensi

---
+++ src/SoloLearn/Arrays/MultidimensionalArrays2.java
@@ -0,0 +1,8 @@
+package SoloLearn.Arrays;
+
+public class MultidimensionalArrays2 {
+    public static void main(String[] args) {
+        String array[][] = {{"Hey", "John", "Bye"},{"John","Johnson","Hello"}};
+        System.out.println(array[1][1]);
+    }
+}
