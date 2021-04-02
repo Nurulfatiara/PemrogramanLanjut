26f2a1745961fe93bdb2bc19a18b4378734526f8
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-02 11:19:24 +0700

    Quiz 3

---
+++ src/SoloLearn/Arrays/Quiz3.java
@@ -0,0 +1,12 @@
+package SoloLearn.Arrays;
+
+public class Quiz3 {
+    public static void main(String[] args) {
+        int arr[] = new int [3];
+    int res = 0;
+    for (int el: arr){
+        res += el;
+    }
+
+    }
+}
