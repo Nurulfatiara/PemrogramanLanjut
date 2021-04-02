70ea72d55e14744263a3fdc045f16f2db16795c8
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-02 21:50:58 +0700

    Getters dan Setters digunakan untuk melindungi data

---
+++ src/SoloLearn/ClassesAndObjects/GettersAndSetters.java
@@ -0,0 +1,11 @@
+package SoloLearn.ClassesAndObjects;
+
+public class GettersAndSetters {
+    private int x;
+    public int getX(){
+        return x;
+    }
+    public void setx(int x){
+        this.x = x;
+    }
+}
