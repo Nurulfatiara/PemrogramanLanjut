0a0bfe7edd3363fb43ac2cac21351dcfdbb56b93
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-02 11:17:09 +0700

    Perulangan for yang disempurnakan

---
+++ src/SoloLearn/Arrays/EnhancedForLoop.java
@@ -0,0 +1,11 @@
+package SoloLearn.Arrays;
+
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        int total = 0;
+        int[ ] arr = {2,3,5,7};
+        for (int x: arr){
+            total += x;
+        }
+    }
+}
