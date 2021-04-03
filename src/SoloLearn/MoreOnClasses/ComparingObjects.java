c146a724b70e930f6c8562b83970aeadc1ade82b
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-03 22:38:07 +0700

    Membandingkan objek

---
+++ src/SoloLearn/MoreOnClasses/ComparingObjects.java
@@ -0,0 +1,12 @@
+package SoloLearn.MoreOnClasses;
+
+public class ComparingObjects {
+        private int x;
+        public static void main(String[] args) {
+         ComparingObjects a = new ComparingObjects();
+         a.x = 5;
+         ComparingObjects b = new ComparingObjects();
+         b.x = 5;
+            System.out.println(a == b);
+        }
+    }
