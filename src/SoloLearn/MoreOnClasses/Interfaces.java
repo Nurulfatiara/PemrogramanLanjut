b9c142aefc85bceeeb2680628b022f01a145340d
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-03 22:28:41 +0700

    Interfaces adalah kelas yang sepenuhnya abstrak yang hanya berisi metode abstrak

---
+++ src/SoloLearn/MoreOnClasses/Interfaces.java
@@ -0,0 +1,12 @@
+package SoloLearn.MoreOnClasses;
+
+public class Interfaces {
+    interface Animal{
+        public void eat();
+    }
+    class Cat implements Animal {
+        public void eat(){
+            System.out.println("Cat eats");
+        }
+    }
+}
