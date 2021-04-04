1e1b36d41c4fd8c798afcae69521d255b8531255
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-04 21:43:03 +0700

    HashMap adalah sebuah class yang berisi sekumpulan value dan key

---
+++ src/SoloLearn/ExceptionsListsThreadsAndFiles/HashMap2.java
@@ -0,0 +1,11 @@
+package SoloLearn.ExceptionsListsThreadsAndFiles;
+
+import java.util.HashMap;
+    class myclass {
+        public static void main(String[] args) {
+            HashMap<String, String> m = new HashMap<String, String>();
+            m.put("A", "First");
+            m.put("B", "Second");
+            System.out.println(m.get("B"));
+        }
+    }
