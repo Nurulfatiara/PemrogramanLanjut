785f76d1fc97fe9b26f7b4769679a606b7bd62f3
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-04 21:35:13 +0700

    Rangkaian eksekusi yang berjalan ketika program dijalankan (Thread)

---
+++ src/SoloLearn/ExceptionsListsThreadsAndFiles/Threads.java
@@ -0,0 +1,14 @@
+package SoloLearn.ExceptionsListsThreadsAndFiles;
+
+public class Threads {
+    static class A extends Threads {
+        public void run() {
+            System.out.println("Hello");
+        }
+
+        public static void main(String[] args) {
+            A object = new A();
+            object.run();
+        }
+    }
+}
