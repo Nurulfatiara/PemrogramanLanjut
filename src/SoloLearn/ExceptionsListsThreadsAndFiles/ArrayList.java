3472550c15d52e8b4ccbb64254305ea0bcff7d2c
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-04 21:37:34 +0700

    Menggunakan ArrayList

---
+++ src/SoloLearn/ExceptionsListsThreadsAndFiles/ArrayList.java
@@ -0,0 +1,7 @@
+package SoloLearn.ExceptionsListsThreadsAndFiles;
+import java.util.ArrayList;
+class array {
+    public static void main(String[] args) {
+        ArrayList<Integer> ar = new ArrayList<Integer>(9);
+    }
+}
