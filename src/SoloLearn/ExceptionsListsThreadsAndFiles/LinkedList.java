86fb30fb14924fd2dffc0e3b298dadde7fdb8ac5
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-04 21:41:17 +0700

    Menggunakan LinkedList

---
+++ src/SoloLearn/ExceptionsListsThreadsAndFiles/LinkedList.java
@@ -0,0 +1,9 @@
+package SoloLearn.ExceptionsListsThreadsAndFiles;
+import java.util.LinkedList;
+    class myClass {
+        public static void main(String[] args) {
+            LinkedList<String> list = new LinkedList<String>();
+            list.add("Hey");
+            System.out.println(list.get(0));
+        }
+    }
