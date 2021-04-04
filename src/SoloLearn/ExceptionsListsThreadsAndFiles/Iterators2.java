ef5794613f13bc1fd99dcf467ea229a2e0feffd3
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-04 21:52:07 +0700

    Iterator adalah objek yang padanya bisa dilakukan iterasi

---
+++ src/SoloLearn/ExceptionsListsThreadsAndFiles/Iterators2.java
@@ -0,0 +1,16 @@
+package SoloLearn.ExceptionsListsThreadsAndFiles;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Iterators2 {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(10);
+        Iterator<Integer> it = list.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+    }
+}
