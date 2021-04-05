f04ae8aa7d5fd48f6ff9542824c92951e3c32f72
Nurulfatiara <12050122431@students.uin-suska.ac.id>
2021-04-05 19:49:56 +0700

    Throw digunakan untuk secara eksplisit melempar sebuah exception

---
+++ src/SoloLearn/ExceptionsListsThreadsAndFiles/Throw.java
@@ -0,0 +1,12 @@
+package SoloLearn.ExceptionsListsThreadsAndFiles;
+
+import java.io.IOException;
+
+public class Throw {
+    public void Do(int x) throws IOException {
+        if (x < 0) {
+            throw new IOException();
+        } else {
+        }
+    }
+}
